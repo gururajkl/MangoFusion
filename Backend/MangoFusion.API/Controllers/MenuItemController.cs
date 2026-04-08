5094d855c0bef7d0dca61003307b45273651b9a4
gururajkl <gururajragavendra7@gmail.com>
2026-04-08 20:11:34 +0530

    Create a menu item controller with getall menuitem endpoint

---
+++ Backend/MangoFusion.API/Controllers/MenuItemController.cs
@@ -0,0 +1,29 @@
+﻿using MangoFusion.API.Data;
+using MangoFusion.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace MangoFusion.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MenuItemController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly ApiResponse _apiResponse;
+
+    public MenuItemController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _apiResponse = new ApiResponse();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetMenuItems()
+    {
+        _apiResponse.Result = await _dbContext.MenuItems.ToListAsync();
+        _apiResponse.StatusCode = HttpStatusCode.OK;
+        return Ok(_apiResponse);
+    }
+}
