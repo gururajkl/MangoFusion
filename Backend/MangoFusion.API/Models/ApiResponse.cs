6bc7ffc4d054d72429e61765d03a177f737860b7
gururajkl <gururajragavendra7@gmail.com>
2026-04-08 19:54:53 +0530

    Create ApiResponse model

---
+++ Backend/MangoFusion.API/Models/ApiResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace MangoFusion.API.Models;
+
+public class ApiResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; }
+    public List<string> ErrorMessage { get; set; } = [];
+    public object? Result { get; set; }
+}
