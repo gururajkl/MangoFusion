04b7e0febeab30abffe83b5c729c5a04742329cd
gururajkl <gururajragavendra7@gmail.com>
2026-04-08 19:27:08 +0530

    Support scalar in backend and update required services

--- Backend/MangoFusion.API/Program.cs
+++ Backend/MangoFusion.API/Program.cs
@@ -1,6 +1,23 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add required services.
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+// Use required services.
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
