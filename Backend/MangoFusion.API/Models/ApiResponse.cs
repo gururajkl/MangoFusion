31f85fc800a774d86acfa431386431b866d16fa5
gururajkl <gururajragavendra7@gmail.com>
2026-04-08 20:11:13 +0530

    Set IsSuccess api prop as true always

--- Backend/MangoFusion.API/Models/ApiResponse.cs
+++ Backend/MangoFusion.API/Models/ApiResponse.cs
@@ -5,7 +5,7 @@ namespace MangoFusion.API.Models;
 public class ApiResponse
 {
     public HttpStatusCode StatusCode { get; set; }
-    public bool IsSuccess { get; set; }
+    public bool IsSuccess { get; set; } = true;
     public List<string> ErrorMessage { get; set; } = [];
     public object? Result { get; set; }
 }
