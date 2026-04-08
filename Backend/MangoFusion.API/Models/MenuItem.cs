47944ae9f16fc6ab1de80acd535c7119b35ac33e
gururajkl <gururajragavendra7@gmail.com>
2026-04-08 19:53:27 +0530

    Create new MenuItem model

---
+++ Backend/MangoFusion.API/Models/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion.API.Models;
+
+public class MenuItem
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string SpecialTag { get; set; } = string.Empty;
+    [Required]
+    public double Price { get; set; }
+    [Required]
+    public string Image { get; set; } = string.Empty;
+}
