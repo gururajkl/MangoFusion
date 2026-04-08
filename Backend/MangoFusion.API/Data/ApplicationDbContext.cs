9c29bd6da6bee7e60940bb47935bab74446b3908
gururajkl <gururajragavendra7@gmail.com>
2026-04-08 19:58:26 +0530

    Add menuitems seed data

--- Backend/MangoFusion.API/Data/ApplicationDbContext.cs
+++ Backend/MangoFusion.API/Data/ApplicationDbContext.cs
@@ -4,4 +4,113 @@ using Microsoft.EntityFrameworkCore;
 
 namespace MangoFusion.API.Data;
 
-public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser>(options) { }
+public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<MenuItem> MenuItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<MenuItem>().HasData(new MenuItem
+        {
+            Id = 1,
+            Name = "Spring Roll",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 7.99,
+            Category = "Appetizer",
+            SpecialTag = ""
+        },
+        new MenuItem
+        {
+            Id = 2,
+            Name = "Idli",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 8.99,
+            Category = "Appetizer",
+            SpecialTag = ""
+        },
+        new MenuItem
+        {
+            Id = 3,
+            Name = "Pani Puri",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 8.99,
+            Category = "Appetizer",
+            SpecialTag = "Best Seller"
+        },
+        new MenuItem
+        {
+            Id = 4,
+            Name = "Hakka Noodles",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 10.99,
+            Category = "Entrée",
+            SpecialTag = ""
+        },
+        new MenuItem
+        {
+            Id = 5,
+            Name = "Malai Kofta",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 12.99,
+            Category = "Entrée",
+            SpecialTag = "Top Rated"
+        },
+        new MenuItem
+        {
+            Id = 6,
+            Name = "Paneer Pizza",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 11.99,
+            Category = "Entrée",
+            SpecialTag = ""
+        },
+        new MenuItem
+        {
+            Id = 7,
+            Name = "Paneer Tikka",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 13.99,
+            Category = "Entrée",
+            SpecialTag = "Chef's Special"
+        },
+        new MenuItem
+        {
+            Id = 8,
+            Name = "Carrot Love",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 4.99,
+            Category = "Dessert",
+            SpecialTag = ""
+        },
+        new MenuItem
+        {
+            Id = 9,
+            Name = "Rasmalai",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 4.99,
+            Category = "Dessert",
+            SpecialTag = "Chef's Special"
+        },
+        new MenuItem
+        {
+            Id = 10,
+            Name = "Sweet Rolls",
+            Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
+            Image = "",
+            Price = 3.99,
+            Category = "Dessert",
+            SpecialTag = "Top Rated"
+        });
+    }
+}
