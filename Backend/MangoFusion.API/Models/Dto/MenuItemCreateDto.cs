0088cb8ab35ac7331317cf5eed7c4bb95491553c
gururajkl <gururajragavendra7@gmail.com>
2026-04-10 18:15:21 +0530

    Create menu item create dto

---
+++ Backend/MangoFusion.API/Models/Dto/MenuItemCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion.API.Models.Dto;
+
+public class MenuItemCreateDto
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string SpecialTag { get; set; } = string.Empty;
+    [Required]
+    public double Price { get; set; }
+    public IFormFile File { get; set; } = default!;
+}
