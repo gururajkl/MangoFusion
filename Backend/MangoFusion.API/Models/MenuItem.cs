f4018e4058b868d5d055fd6e9d73ecae18920178
gururajkl <gururajragavendra7@gmail.com>
2026-04-10 18:21:37 +0530

    Update MenuItem model and make some props as nullable

--- Backend/MangoFusion.API/Models/MenuItem.cs
+++ Backend/MangoFusion.API/Models/MenuItem.cs
@@ -8,9 +8,9 @@ public class MenuItem
     public int Id { get; set; }
     [Required]
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; }
     public string Category { get; set; } = string.Empty;
-    public string SpecialTag { get; set; } = string.Empty;
+    public string? SpecialTag { get; set; }
     [Required]
     public double Price { get; set; }
     [Required]
