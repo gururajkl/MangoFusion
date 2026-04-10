2a9ba34e7a6299948c178fc71755797e166b3a73
gururajkl <gururajragavendra7@gmail.com>
2026-04-10 18:19:26 +0530

    Update menuitem create and update dto

--- Backend/MangoFusion.API/Models/Dto/MenuItemCreateDto.cs
+++ Backend/MangoFusion.API/Models/Dto/MenuItemCreateDto.cs
@@ -6,9 +6,9 @@ public class MenuItemCreateDto
 {
     [Required]
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; }
     public string Category { get; set; } = string.Empty;
-    public string SpecialTag { get; set; } = string.Empty;
+    public string? SpecialTag { get; set; }
     [Required]
     public double Price { get; set; }
     public IFormFile File { get; set; } = default!;
--- Backend/MangoFusion.API/Models/Dto/MenuItemUpdateDto.cs
+++ Backend/MangoFusion.API/Models/Dto/MenuItemUpdateDto.cs
@@ -8,10 +8,10 @@ public class MenuItemUpdateDto
     public int Id { get; set; }
     [Required]
     public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; }
     public string Category { get; set; } = string.Empty;
-    public string SpecialTag { get; set; } = string.Empty;
+    public string? SpecialTag { get; set; }
     [Required]
     public double Price { get; set; }
-    public IFormFile File { get; set; } = default!;
+    public IFormFile? File { get; set; }
 }
