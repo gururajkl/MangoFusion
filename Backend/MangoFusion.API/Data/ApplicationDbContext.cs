119eb11d9467f3eb2319e1dab044b9991d40032e
gururajkl <gururajragavendra7@gmail.com>
2026-04-10 22:08:26 +0530

    Update application dbContext with images

--- Backend/MangoFusion.API/Data/ApplicationDbContext.cs
+++ Backend/MangoFusion.API/Data/ApplicationDbContext.cs
@@ -17,27 +17,27 @@ public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<
             Id = 1,
             Name = "Spring Roll",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/spring roll.jpg",
             Price = 7.99,
             Category = "Appetizer",
-            SpecialTag = ""
+            SpecialTag = "images/spring roll.jpg"
         },
         new MenuItem
         {
             Id = 2,
             Name = "Idli",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/Idli.jpg",
             Price = 8.99,
             Category = "Appetizer",
-            SpecialTag = ""
+            SpecialTag = "images/spring roll.jpg"
         },
         new MenuItem
         {
             Id = 3,
             Name = "Pani Puri",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/pani puri.jpg",
             Price = 8.99,
             Category = "Appetizer",
             SpecialTag = "Best Seller"
@@ -47,17 +47,17 @@ public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<
             Id = 4,
             Name = "Hakka Noodles",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/hakka noodles.jpg",
             Price = 10.99,
             Category = "Entrée",
-            SpecialTag = ""
+            SpecialTag = "images/spring roll.jpg"
         },
         new MenuItem
         {
             Id = 5,
             Name = "Malai Kofta",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/malai kofta.jpg",
             Price = 12.99,
             Category = "Entrée",
             SpecialTag = "Top Rated"
@@ -70,14 +70,14 @@ public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<
             Image = "",
             Price = 11.99,
             Category = "Entrée",
-            SpecialTag = ""
+            SpecialTag = "images/paneer pizza.jpg"
         },
         new MenuItem
         {
             Id = 7,
             Name = "Paneer Tikka",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/paneer tikka.jpg",
             Price = 13.99,
             Category = "Entrée",
             SpecialTag = "Chef's Special"
@@ -87,17 +87,17 @@ public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<
             Id = 8,
             Name = "Carrot Love",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/spring roll.jpg",
             Price = 4.99,
             Category = "Dessert",
-            SpecialTag = ""
+            SpecialTag = "images/carrot love.jpg"
         },
         new MenuItem
         {
             Id = 9,
             Name = "Rasmalai",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/rasmalai.jpg",
             Price = 4.99,
             Category = "Dessert",
             SpecialTag = "Chef's Special"
@@ -107,7 +107,7 @@ public class ApplicationDbContext(DbContextOptions options) : IdentityDbContext<
             Id = 10,
             Name = "Sweet Rolls",
             Description = "Fusc tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.",
-            Image = "",
+            Image = "images/sweet rolls.jpg",
             Price = 3.99,
             Category = "Dessert",
             SpecialTag = "Top Rated"
