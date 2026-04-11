055bf0c65e0b56cd0872651a3a3e1aa42e8f67b2
gururajkl <gururajragavendra7@gmail.com>
2026-04-11 18:53:10 +0530

    Update RegisterRequestDto

--- Backend/MangoFusion.API/Models/Dto/RegisterRequestDto.cs
+++ Backend/MangoFusion.API/Models/Dto/RegisterRequestDto.cs
@@ -9,6 +9,6 @@ public class RegisterRequestDto
     [Required]
     public string Password { get; set; } = string.Empty;
     [Required]
-    public string ConfirmPassword { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
 }
