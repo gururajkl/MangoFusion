3ab568a8d1dab4891ce2e62e4e63f7a81b3c7127
gururajkl <gururajragavendra7@gmail.com>
2026-04-11 18:38:52 +0530

    Create LoginRequestDto

---
+++ Backend/MangoFusion.API/Models/Dto/LoginRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace MangoFusion.API.Models.Dto;
+
+public class LoginRequestDto
+{
+    public string UserEmail { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
