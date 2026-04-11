30f9b992f8e49dd645c3b0eac002abab1ef1cfe8
gururajkl <gururajragavendra7@gmail.com>
2026-04-11 18:40:26 +0530

    Update LoginRequestDto

--- Backend/MangoFusion.API/Models/Dto/LoginRequestDto.cs
+++ Backend/MangoFusion.API/Models/Dto/LoginRequestDto.cs
@@ -2,6 +2,6 @@
 
 public class LoginRequestDto
 {
-    public string UserEmail { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
