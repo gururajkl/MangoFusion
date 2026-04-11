dc1ddcf016a216c41da69e93cbb9aae8a425ba01
gururajkl <gururajragavendra7@gmail.com>
2026-04-11 18:43:38 +0530

    Create RegisterRequestDto

---
+++ Backend/MangoFusion.API/Models/Dto/RegisterRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion.API.Models.Dto;
+
+public class RegisterRequestDto
+{
+    [Required]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+    [Required]
+    public string ConfirmPassword { get; set; } = string.Empty;
+    public string Role { get; set; } = string.Empty;
+}
