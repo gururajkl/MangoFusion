0415c0d6e490dbbae5c9b85639c8c53526def288
gururajkl <gururajragavendra7@gmail.com>
2026-04-11 20:05:32 +0530

    Implement user registration in AuthController
    Refactored AuthController to add a Register endpoint for user creation, role assignment, and role management. Injected UserManager, RoleManager, and configuration for dependency handling. Standardized API responses with ApiResponse. Removed previous GET endpoints and related authorization.

--- Backend/MangoFusion.API/Controllers/AuthController.cs
+++ Backend/MangoFusion.API/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
-﻿using MangoFusion.API.Utility;
-using Microsoft.AspNetCore.Authorization;
+﻿using MangoFusion.API.Models;
+using MangoFusion.API.Models.Dto;
+using MangoFusion.API.Utility;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace MangoFusion.API.Controllers;
 
@@ -8,17 +11,95 @@ namespace MangoFusion.API.Controllers;
 [Route("api/[controller]")]
 public class AuthController : Controller
 {
-    [HttpGet]
-    [Authorize]
-    public ActionResult<string> GetSomething()
+    private readonly ApiResponse _apiResponse;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly string _secretKey;
+
+    public AuthController(UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager, IConfiguration configuration)
     {
-        return "You are authorized";
+        _apiResponse = new();
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _secretKey = configuration.GetValue<string>("ApiSettings:Secret")!;
     }
 
-    [HttpGet("{someValue:int}")]
-    [Authorize(Roles = StaticDetails.RoleAdmin)]
-    public ActionResult<string> GetSomething(int someValue)
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDto model)
     {
-        return $"You are authorized with admin role {someValue}";
+        if (ModelState.IsValid)
+        {
+            ApplicationUser newUser = new()
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                Name = model.Name,
+                NormalizedEmail = model.Email.ToUpper()
+            };
+
+            // Create new user using UserManager.
+            var result = await _userManager.CreateAsync(newUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                // Check if role exists, if not create that role.
+                await CheckAndCreateRoles();
+
+                // Now let's assign the role for the user.
+                await AssignRoleToTheUser(model, newUser);
+
+                _apiResponse.StatusCode = HttpStatusCode.OK;
+                _apiResponse.IsSuccess = true;
+                return Ok(_apiResponse);
+            }
+            else
+            {
+                _apiResponse.StatusCode = HttpStatusCode.BadRequest;
+                _apiResponse.IsSuccess = false;
+                foreach (var error in result.Errors)
+                {
+                    _apiResponse.ErrorMessage.Add(error.Description);
+                }
+
+                return BadRequest(_apiResponse);
+            }
+        }
+        else
+        {
+            _apiResponse.StatusCode = HttpStatusCode.BadRequest;
+            _apiResponse.IsSuccess = false;
+
+            foreach (var value in ModelState.Values)
+            {
+                foreach (var error in value.Errors)
+                {
+                    _apiResponse.ErrorMessage.Add(error.ErrorMessage);
+                }
+            }
+
+            return BadRequest(_apiResponse);
+        }
+    }
+
+    private async Task CheckAndCreateRoles()
+    {
+        if (!await _roleManager.RoleExistsAsync(StaticDetails.RoleAdmin))
+        {
+            await _roleManager.CreateAsync(new IdentityRole(StaticDetails.RoleAdmin));
+            await _roleManager.CreateAsync(new IdentityRole(StaticDetails.RoleCustomer));
+        }
+    }
+
+    private async Task AssignRoleToTheUser(RegisterRequestDto model, ApplicationUser newUser)
+    {
+        if (model.Role.Equals(StaticDetails.RoleAdmin, StringComparison.OrdinalIgnoreCase))
+        {
+            await _userManager.AddToRoleAsync(newUser, StaticDetails.RoleAdmin);
+        }
+        else
+        {
+            await _userManager.AddToRoleAsync(newUser, StaticDetails.RoleCustomer);
+        }
     }
 }
