a9d9614d8e986c083d811a8f9fe4f6013e250227
gururajkl <gururajragavendra7@gmail.com>
2026-04-11 13:36:36 +0530

    Create AuthController

---
+++ Backend/MangoFusion.API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using MangoFusion.API.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangoFusion.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : Controller
+{
+    [HttpGet]
+    [Authorize]
+    public ActionResult<string> GetSomething()
+    {
+        return "You are authorized";
+    }
+
+    [HttpGet("{someValue:int}")]
+    [Authorize(Roles = StaticDetails.RoleAdmin)]
+    public ActionResult<string> GetSomething(int someValue)
+    {
+        return $"You are authorized with admin role {someValue}";
+    }
+}
