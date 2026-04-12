6526cf5b7520c6e574833b69eb64aa16d1a16438
gururajkl <gururajragavendra7@gmail.com>
2026-04-12 12:52:24 +0530

    Fix const placements

--- Backend/MangoFusion.API/Utility/StaticDetails.cs
+++ Backend/MangoFusion.API/Utility/StaticDetails.cs
@@ -5,7 +5,7 @@ public static class StaticDetails
     public const string RoleAdmin = "Admin";
     public const string RoleCustomer = "Customer";
     public const string StatusConfirmed = "Confirmed";
+    public const string StatusReadyForPickUp = "Ready for Pickup";
     public const string StatusCompleted = "Completed";
     public const string StatusCancelled = "Cancelled";
-    public const string StatusReadyForPickUp = "Ready for Pickup";
 }
