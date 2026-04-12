61999435a28395a4ce1b9dca641347fcd1a72389
gururajkl <gururajragavendra7@gmail.com>
2026-04-12 07:21:34 +0530

    Add OrderHeaderUpdateDto

---
+++ Backend/MangoFusion.API/Models/Dto/OrderHeaderUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion.API.Models.Dto;
+
+public class OrderHeaderUpdateDto
+{
+    [Key]
+    public int OrderHeaderId { get; set; }
+    [Required]
+    public string PickUpName { get; set; } = string.Empty;
+    [Required]
+    public string PickUpPhoneNumber { get; set; } = string.Empty;
+    [Required]
+    public string PickUpEmail { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
+}
