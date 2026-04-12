27e1897c16662493377c2afb0f293994a564c1f8
gururajkl <gururajragavendra7@gmail.com>
2026-04-12 18:43:29 +0530

    Add ROUTES constant for application navigation

---
+++ Frontend/mangopedia/src/utility/constants.js
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  CART: "/cart",
+  CHECKOUT: "/checkout",
+  ORDER_CONFIRMATION: "/order-confirmation",
+  ORDER_MANAGEMENT: "/order-management",
+  MENU_MANAGEMENT: "/menu-management",
+};
