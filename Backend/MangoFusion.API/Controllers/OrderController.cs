27a8a2cbd7c42cdad30da74cc6a07ac92394a79b
gururajkl <gururajragavendra7@gmail.com>
2026-04-12 07:24:24 +0530

    Add OrderController
    This is just a initial change

---
+++ Backend/MangoFusion.API/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using MangoFusion.API.Data;
+using MangoFusion.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangoFusion.API.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+    private readonly ApiResponse _apiResponse;
+
+    public OrderController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _apiResponse = new();
+    }
+}
