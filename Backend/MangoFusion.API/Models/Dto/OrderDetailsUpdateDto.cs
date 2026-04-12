5930b2da938f78e1444105eb4c6339c2b6f4bd26
gururajkl <gururajragavendra7@gmail.com>
2026-04-12 14:41:34 +0530

    Update range attribute for the ratings

--- Backend/MangoFusion.API/Models/Dto/OrderDetailsUpdateDto.cs
+++ Backend/MangoFusion.API/Models/Dto/OrderDetailsUpdateDto.cs
@@ -5,7 +5,7 @@ namespace MangoFusion.API.Models.Dto;
 public class OrderDetailsUpdateDto
 {
     [Required]
-    public int MenuItemId { get; set; }
+    public int OrderDetailsId { get; set; }
     [Required]
     [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
     public int Rating { get; set; }
