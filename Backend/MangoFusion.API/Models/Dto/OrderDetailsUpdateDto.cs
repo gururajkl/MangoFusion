8d0f758eda8c27503d1f1c6edd8de9e8d5b4911d
gururajkl <gururajragavendra7@gmail.com>
2026-04-12 14:39:46 +0530

    Update OrderDetailsUpdateDto

--- Backend/MangoFusion.API/Models/Dto/OrderDetailsUpdateDto.cs
+++ Backend/MangoFusion.API/Models/Dto/OrderDetailsUpdateDto.cs
@@ -7,7 +7,6 @@ public class OrderDetailsUpdateDto
     [Required]
     public int MenuItemId { get; set; }
     [Required]
-    public int OrderDetailsId { get; set; }
-    [Required]
+    [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
     public int Rating { get; set; }
 }
