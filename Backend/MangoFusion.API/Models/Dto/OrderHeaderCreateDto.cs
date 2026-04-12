ee43d88014b286d040f64b8c83c0b8ce01185b8a
gururajkl <gururajragavendra7@gmail.com>
2026-04-12 07:21:52 +0530

    Add OrderHeaderCreateDto

---
+++ Backend/MangoFusion.API/Models/Dto/OrderHeaderCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion.API.Models.Dto;
+
+public class OrderHeaderCreateDto
+{
+    [Required]
+    public string PickUpName { get; set; } = string.Empty;
+    [Required]
+    public string PickUpPhoneNumber { get; set; } = string.Empty;
+    [Required]
+    public string PickUpEmail { get; set; } = string.Empty;
+    public double OrderTotal { get; set; }
+    public string Status { get; set; } = string.Empty;
+    public int TotalItems { get; set; }
+    public List<OrderDetailsCreateDto> OrderDetailsCreateDtos { get; set; } = [];
+}
