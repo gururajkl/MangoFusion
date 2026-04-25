2ef4289d57aaf718065f5b3ffb4cc0bcf5e88d5d
gururajkl <gururajragavendra7@gmail.com>
2026-04-25 16:01:23 +0530

    Refactor token and user retrieval in getInitialAuthState for clarity

--- Frontend/mangopedia/src/components/store/slice/authSlice.js
+++ Frontend/mangopedia/src/components/store/slice/authSlice.js
@@ -2,14 +2,14 @@ import { STORAGE_KEYS } from "../../../utility/constants";
 import { isTokenExpired } from "../../../utility/jwtUtility";
 
 const getInitialAuthState = () => {
-  const token = localStorage.getItem(STORAGE_KEYS.TOKENMANGO);
-  const user = localStorage.getItem(STORAGE_KEYS.USER);
+  const storedToken = localStorage.getItem(STORAGE_KEYS.TOKENMANGO);
+  const storedUser = localStorage.getItem(STORAGE_KEYS.USER);
 
   if (
-    !token ||
-    token === "undefined" ||
-    token === "null" ||
-    isTokenExpired(token)
+    !storedToken ||
+    storedToken === "undefined" ||
+    storedToken === "null" ||
+    isTokenExpired(storedToken)
   ) {
     localStorage.removeItem(STORAGE_KEYS.TOKENMANGO);
     localStorage.removeItem(STORAGE_KEYS.USER);
