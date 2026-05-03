1aee359874c996154da6e655408233995010f251
gururajkl <gururajragavendra7@gmail.com>
2026-05-03 08:29:53 +0530

    Update store with theme reducer

--- Frontend/mangopedia/src/components/store/store.js
+++ Frontend/mangopedia/src/components/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/baseApi";
 import authReducer from "./slice/authSlice";
 import cartReducer from "./slice/cartSlice";
+import themeReducer from "./slice/themeSlice";
 
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
     auth: authReducer,
     cart: cartReducer,
+    theme: themeReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
